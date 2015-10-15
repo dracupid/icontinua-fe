be9ede0166d2dc64ec1b32fd18d46d4843df92ab
Zhao Jingchen <dracupid@gmail.com>
2015-10-15 20:52:23 +0800

    部署脚本

---
+++ bak_static.sh
@@ -0,0 +1,6 @@
+cd /var/www
+if [ ! -d bak/static ]
+then
+    mkdir -p bak/static
+fi
+tar --exclude="*.tar.bz2" -jcf bak/static/$(date "+%Y-%m-%d_%H-%M-%S").tar.bz2 static
---
+++ deploy.sh
@@ -0,0 +1,24 @@
+root='/var/www/'
+host='wcm@172.18.9.7'
+if [ ! -d upload ]
+then
+    mkdir upload
+fi
+cd dist
+echo "Packing..."
+tar --exclude="**/.DS_Store" -jcf ../upload/static.tar.bz2 *
+
+echo "Backup files on the server..."
+ssh $host "sh ${root}bak_static.sh"
+
+echo "Uploading..."
+cd ..
+scp -r upload/static.tar.bz2 ${host}:${root}static
+
+echo "Unpacking..."
+ssh $host "cd ${root}static && tar -xjf static.tar.bz2"
+
+echo "Cleaning..."
+rm -rf upload
+
+echo "Done!"
