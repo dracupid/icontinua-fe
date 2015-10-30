fbe02c97c24439df25e1e39ea17ff26d0844ff45
Zhao Jingchen <dracupid@gmail.com>
2015-10-30 12:43:49 +0800

    预读取图片

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -7,10 +7,24 @@ window._reportListData = null;
 window._chineseReportData = {};
 window._advice = null;
 
+(function () {
+    let cacheImg = function (url) {
+        let img = new Image();
+        img.src = url;
+    };
+    cacheImg("/img/loading.gif");
+    cacheImg("/img/record.png");
+    cacheImg("/img/trade.png");
+    cacheImg("/img/trade-a.png");
+    cacheImg("/img/body.png");
+    cacheImg("/img/wave.png");
+})();
+
 React.render((
     <Router >
-        <Route path="/" component={reports} />
+        <Route path="/" component={reports}/>
         <Route path="/:openId" component={reports}/>
+        <Route path="/share/:reportId" component={report}/>
         <Route path="/:openId/:reportId" component={report}/>
     </Router>
 ), document.body);
