dcd952fbe193b5f05228b41b20d418555eb402a3
Zhao Jingchen <dracupid@gmail.com>
2015-12-23 10:52:24 +0800

    opt ant-design build

--- src/antd.js
+++ src/antd.js
@@ -1,1 +1,11 @@
-window.ANTD = require('antd');
+'use strict';
+
+window.ANTD = {
+  Affix: require('../node_modules/antd/lib/affix'),
+  Tabs: require('../node_modules/antd/lib/tabs'),
+  Modal: require('../node_modules/antd/lib/modal'),
+  Popconfirm: require('../node_modules/antd/lib//popconfirm'),
+  message: require('../node_modules/antd/lib/message'),
+  Alert: require('../node_modules/antd/lib/alert'),
+  Spin: require('../node_modules/antd/lib/spin')
+};
