c8280b8547ac82dd60bbc6e738c1181888b976ae
Zhao Jingchen <dracupid@gmail.com>
2016-03-02 16:40:48 +0800

    增加用户头像显示

--- src/jsx/Components/Banner.jsx
+++ src/jsx/Components/Banner.jsx
@@ -9,13 +9,15 @@ function Banner (props) {
         }
       })()}
       <h2>{props.title}</h2>
+      {props.rightComponent}
     </div>
   )
 }
 
 Banner.propTypes = {
   title: React.PropTypes.string.isRequired,
-  backUrl: React.PropTypes.string
+  backUrl: React.PropTypes.string,
+  rightComponent: React.PropTypes.object
 }
 
 export default Banner
