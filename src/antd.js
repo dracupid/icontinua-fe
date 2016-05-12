b997c5abd2cd8a98b1bdc6e0278d3a1c6d90cd42
Zhao Jingchen <dracupid@gmail.com>
2016-05-12 14:42:14 +0800

    remove unused antd components

--- src/antd.js
+++ src/antd.js
@@ -1,14 +1,12 @@
 'use strict'
 
 window.ANTD = {
-  Affix: require('../node_modules/antd/lib/affix'),
   Tabs: require('../node_modules/antd/lib/tabs'),
   Modal: require('../node_modules/antd/lib/modal'),
   Popconfirm: require('../node_modules/antd/lib/popconfirm'),
   message: require('../node_modules/antd/lib/message'),
   Alert: require('../node_modules/antd/lib/alert'),
   Spin: require('../node_modules/antd/lib/spin'),
-  Popover: require('../node_modules/antd/lib/popover'),
   Icon: require('../node_modules/antd/lib/icon'),
   Button: require('../node_modules/antd/lib/button'),
   Form: require('../node_modules/antd/lib/form'),
