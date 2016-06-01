2c267afb0eeca56adbd69af0d939e509ce6005b2
Zhao Jingchen <dracupid@gmail.com>
2016-06-01 23:47:30 +0800

    opt

--- src/antd.js
+++ src/antd.js
@@ -3,7 +3,6 @@
 window.ANTD = {
   Tabs: require('../node_modules/antd/lib/tabs'),
   Modal: require('../node_modules/antd/lib/modal'),
-  Popconfirm: require('../node_modules/antd/lib/popconfirm'),
   message: require('../node_modules/antd/lib/message'),
   Alert: require('../node_modules/antd/lib/alert'),
   Spin: require('../node_modules/antd/lib/spin'),
