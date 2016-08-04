ba37a268cb0607c5504dc08afc7793f9277639a3
Zhao Jingchen <dracupid@gmail.com>
2016-08-04 16:23:47 +0800

    update

--- src/jsx/Components/Footer.jsx
+++ src/jsx/Components/Footer.jsx
@@ -2,7 +2,7 @@
  * 页脚组件
  */
 function Footer (props) {
-  return <footer {...props}>© 2016 杭州铭唐信息科技有限公司 | 浙ICP备16007076号</footer>
+  return <footer {...props}>© 2016 杭州佑唐信息科技有限公司 | 浙ICP备16007076号</footer>
 }
 
 export default Footer
