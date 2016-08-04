bec39938d3a0581ad9ad775efcb0f7076a308fa7
Zhao Jingchen <dracupid@gmail.com>
2016-08-04 17:33:34 +0800

    允许用户自定义页脚

--- src/jsx/Components/Footer.jsx
+++ src/jsx/Components/Footer.jsx
@@ -1,7 +1,14 @@
 /**
  * 页脚组件
  */
+import util from '../util.jsx'
+
 function Footer (props) {
+  if (util.getParam('channel') === 'HuiLianHuZhou') {
+    let style = _.clone(props.style) || {}
+    style.lineHeight = 'inherit'
+    return <footer {...props} style={style}>湖州惠联营销策划有限公司 浙ICP备15025197号-1 <br/> 联系电话：18606829979</footer>
+  }
   return <footer {...props}>© 2016 杭州佑唐信息科技有限公司 | 浙ICP备16007076号</footer>
 }
 
