a6be081e452fa0451302ca93b6d9e42d58f861a0
Zhao Jingchen <dracupid@gmail.com>
2016-11-15 15:06:17 +0800

    增加创建二维码功能

--- src/jsx/API/admin.jsx
+++ src/jsx/API/admin.jsx
@@ -5,5 +5,11 @@ export default {
   listDevice: () => API('/api/admin/device/list'),
   statDevice: (did) => API('/api/admin/device/stat?id=' + did),
   statChannel: (name) => API('/api/admin/channel/stat?name=' + name),
-  deleteChannel: (name) => API('/api/admin/channel/delete?name=' + name)
+  deleteChannel: (name) => API('/api/admin/channel/delete?name=' + name),
+  createChannel: (name) => {
+    if (!name) {
+      return Promise.reject()
+    }
+    return API('/api/admin/channel/create?name=' + name)
+  }
 }
--- src/jsx/admin/ChannelCreate.jsx
+++ src/jsx/admin/ChannelCreate.jsx
@@ -1,5 +1,59 @@
-export default class Create extends React.Component {
+import Form from 'antd/lib/form/index.js'
+import Input from 'antd/lib/input/index.js'
+import Button from 'antd/lib/button/index.js'
+import Row from 'antd/lib/row/index.js'
+import Col from 'antd/lib/col/index.js'
+import API from '../API/admin.jsx'
+
+class Create extends React.Component {
+  state = {
+    img: "",
+    name: ""
+  }
+
+  handleSubmit (e) {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        API.createChannel(values.name)
+          .then((img) => {
+            this.setState({img, name: values.name})
+          })
+      }
+    });
+  }
+
   render () {
-    return <div>未完成</div>
+    const {getFieldDecorator} = this.props.form;
+    return (
+      <Row type="flex">
+        <Col span={12}>
+          <Form onSubmit={::this.handleSubmit} className="login-form" style={{width: '100%'}}>
+            <Form.Item label="渠道名称" labelCol={{span: 6}} wrapperCol={{span: 14}}>
+              {getFieldDecorator('name', {
+                rules: [{required: true, message: '渠道名称未填写'}],
+              })(
+                <Input placeholder="不能超过64个字母"/>
+              )}
+            </Form.Item>
+            <Form.Item wrapperCol={{span: 14, offset: 6}}>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                创建
+              </Button>
+            </Form.Item>
+          </Form>
+        </Col>
+        {this.state.name && this.state.img
+          ? <Col span={12}>
+           <img src={this.state.img} style={{width: 280}}/>
+           <p style={{fontSize: 18}}>{`↑ 渠道${this.state.name}的二维码️ ↑`}</p>
+         </Col>
+          : null
+        }
+
+      </Row>
+    );
   }
 }
+
+export default Form.create()(Create)
