7ec3346fdc441d0bbd8f20cc41752b5b88d80cad
Zhao Jingchen <dracupid@gmail.com>
2017-05-04 11:21:02 +0800

    对于某些设备上的测量，只显示生物电结果

--- src/jsx/API/report.jsx
+++ src/jsx/API/report.jsx
@@ -7,17 +7,26 @@ export default {
    * @param sid 报告shortId
    */
   report: (id, sid) => {
+    let promise
     if (id) {
-      return API('/api/report?rank=true&diagnose=true&reportId=' + id)
+      promise = API('/api/report?rank=true&diagnose=true&reportId=' + id)
     } else if (sid) {
       sid = sid.trim().replace(/[A-Z]/g, '') // 没有大写字母
       // if (sid.indexOf('AKT') === 0) {
       //   sid = sid.slice(3)
       // }
-      return API('/api/report-sid?sid=' + sid)
+      promise = API('/api/report-sid?sid=' + sid)
     } else {
       return Promise.reject(new Error('reportId not provided.'))
     }
+
+    return promise.then((data) => {
+      // channel 包含 only-chinese，只显示生物电结果
+      if (data.channel.indexOf('only-chinese') >= 0) {
+        let {id, location, channel, sid, timestamp, cacheId, user, jizhui, zangfu, xiaohua, miniao} = data
+        return {id, location, channel, sid, timestamp, cacheId, user, jizhui, zangfu, xiaohua, miniao}
+      }
+    })
   },
   /**
    * 获取报告列表
